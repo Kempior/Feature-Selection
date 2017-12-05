9b9f50875f66a359b4dd1b7fda47b05ad2e4d3ff
Kempior <kempiorr@gmail.com>
2017-12-05 16:01:23 +0100

    Project and git files

---
+++ FeatureSelection/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeatureSelection
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
