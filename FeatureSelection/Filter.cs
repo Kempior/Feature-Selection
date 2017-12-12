43ab9ca83b8148c41db70334df4fd654637661ad
MustardSpaceship <wojtek4p4@gmail.com>
2017-12-12 17:21:30 +0100

    Added the Filter abstract class

---
+++ FeatureSelection/Filter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeatureSelection
+{
+	abstract class Filter
+	{
+		public abstract double[][] FilterData(double[][] data, uint remaining);
+		public double[][] FilterData(double[][] data)
+		{
+			return FilterData(data, 0);
+		}
+	}
+}
